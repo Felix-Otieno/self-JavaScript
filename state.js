17069d31f48888737faf69cb58f2952fb875421e
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-07-09 18:05:05 +0300

    my first commit

---
+++ state.js
@@ -0,0 +1,5 @@
+let x, y, z;
+x = 20;
+y = 50;
+z = x + y;
+console.log("the value of z "+z+"")
---
+++ statement.js
@@ -0,0 +1,5 @@
+let a, b, c; 
+a = 2;
+b = 3;
+c = a + b;
+console.log("the value of c is "+ c +".");
