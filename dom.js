06f90b5982b1aac2977d7852beaf4b96999c7c6e
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-07-10 18:53:32 +0300

    add dom

---
+++ dom.js
@@ -0,0 +1,11 @@
+// document.getElementById("leo").innerHTML="Today I'm happy"
+
+const leo = document.getElementById("leo");
+leo.innerHTML = "Work hard please!";
+
+// create new element 
+
+// const NewItem = document.getElementById("h1");
+// const HeadingTitle = document.createTextNode("The day will come");
+
+
